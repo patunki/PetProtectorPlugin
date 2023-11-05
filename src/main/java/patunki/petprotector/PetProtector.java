d369449e9d9dbff8866d604a9b91d75ffac36856
patunki <eeronpoika.haapala@gmail.com>
2023-11-05 17:25:27 +0200

    Typo fix.

--- src/main/java/patunki/petprotector/PetProtector.java
+++ src/main/java/patunki/petprotector/PetProtector.java
@@ -1,14 +1,13 @@
 package patunki.petprotector;
 
-import org.bukkit.entity.Entity;
-import org.bukkit.entity.Player;
+
 import org.bukkit.entity.Tameable;
-import org.bukkit.entity.TextDisplay;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageEvent;
-import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.Objects;
 
 public final class PetProtector extends JavaPlugin implements Listener {
 
@@ -29,21 +28,10 @@ public final class PetProtector extends JavaPlugin implements Listener {
         Tameable a = (Tameable) event.getEntity();
         if (a.isTamed()){
             event.setCancelled(true);
-            System.out.println("This pet is protected by " + a.getOwner().getName() );
+            System.out.println("Pet avoided damage. Protected by " + Objects.requireNonNull(a.getOwner()).getName() );
+
         }
 
 
     }
-
-
-
-
-
-    @Override
-    public void onDisable() {
-        //System.out.println("PetProtector has stopped.");
-    }
 }
-
-//[16:31:39 INFO]: CraftWolf{owner=null,tamed=false}
-//[16:31:54 INFO]: CraftWolf{owner=CraftPlayer{name=},tamed=true}
