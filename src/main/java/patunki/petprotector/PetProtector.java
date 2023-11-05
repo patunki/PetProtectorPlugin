7ea69a1e5c04699895a573463b8779bf6f05856a
Paulus <96471980+patunki@users.noreply.github.com>
2023-11-05 18:37:50 +0200

    Update PetProtector.java

--- src/main/java/patunki/petprotector/PetProtector.java
+++ src/main/java/patunki/petprotector/PetProtector.java
@@ -1,14 +1,13 @@
 package patunki.petprotector;
 
-import org.bukkit.entity.Entity;
-import org.bukkit.entity.Player;
+
 import org.bukkit.entity.Tameable;
-import org.bukkit.entity.TextDisplay;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageEvent;
-import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.Objects;
 
 public final class PetProtector extends JavaPlugin implements Listener {
 
@@ -16,25 +15,27 @@ public final class PetProtector extends JavaPlugin implements Listener {
     @Override
     public void onEnable() {
         System.out.println("Pet Protector starting up.");
-        System.out.println("Thanks to BooyaTheSkeleton on Reddit!");
-
+        //Thanks to BooyaTheSkeleton on Reddit!
         getServer().getPluginManager().registerEvents(this, this);
 
-        System.out.println("PetProtector started.");
+
     }
 
 
     @EventHandler
     public void onEntityDamage(EntityDamageEvent event){
-        Tameable a = (Tameable) event.getEntity();
-        if (a.isTamed()){
-            event.setCancelled(true);
-            System.out.println("This pet is protected by " + a.getOwner().getName() );
-        }
 
+            try{
+                Tameable a = (Tameable) event.getEntity();
+                if (a.isTamed()){
+                    event.setCancelled(true);
+                    System.out.println("Pet avoided damage. Protected by " + Objects.requireNonNull(a.getOwner()).getName() );
+
+                }
+            }
+            catch(Exception ignored) {
+
+            }
 
     }
-
-
 }
-
