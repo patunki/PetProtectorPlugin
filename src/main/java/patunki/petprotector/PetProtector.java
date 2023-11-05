8cf3e729d07d443554fa65bc347de99eb39c2872
patunki <eeronpoika.haapala@gmail.com>
2023-11-05 18:36:09 +0200

    Typo fix.

--- src/main/java/patunki/petprotector/PetProtector.java
+++ src/main/java/patunki/petprotector/PetProtector.java
@@ -15,23 +15,27 @@ public final class PetProtector extends JavaPlugin implements Listener {
     @Override
     public void onEnable() {
         System.out.println("Pet Protector starting up.");
-        System.out.println("Thanks to BooyaTheSkeleton on Reddit!");
-
+        //Thanks to BooyaTheSkeleton on Reddit!
         getServer().getPluginManager().registerEvents(this, this);
 
-        System.out.println("PetProtector started.");
+
     }
 
 
     @EventHandler
     public void onEntityDamage(EntityDamageEvent event){
-        Tameable a = (Tameable) event.getEntity();
-        if (a.isTamed()){
-            event.setCancelled(true);
-            System.out.println("Pet avoided damage. Protected by " + Objects.requireNonNull(a.getOwner()).getName() );
 
-        }
+            try{
+                Tameable a = (Tameable) event.getEntity();
+                if (a.isTamed()){
+                    event.setCancelled(true);
+                    System.out.println("Pet avoided damage. Protected by " + Objects.requireNonNull(a.getOwner()).getName() );
 
+                }
+            }
+            catch(Exception ignored) {
+
+            }
 
     }
 }
